740438263fa45045e2409d615a5c8711c578cfa2
Jasmine Reyes <jasminereyess44@gmail.com>
2022-03-28 10:57:56 -0700

    different port

--- server.js
+++ server.js
@@ -9,9 +9,12 @@ app.get('/', function(request, response) {
     response.sendFile('public/components/Desktop/desktop.html', {root: '.'});
 });
 
-app.listen(3000, () => {
-    console.log(`Running on: http://localhost:${3000}`)
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Running on server now.`)
 });
 
-
+// app.listen(3000, () => {
+//     console.log(`Running on: http://localhost:${3000}`)
+// });
 //hello
